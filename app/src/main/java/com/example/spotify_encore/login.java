fda6897ee2cffc9115eedc38b59bf60728bc89c2
Nguyen, Dennis Duy Quang <dennisnguyen1475@gmail.com>
2024-04-08 14:47:24 -0400

    The only issue here is that the button for signing up does not work so yea
    Methods:
    onClicksignup
    
    Files:
    SignupActivity // File for signup view
    AndroidManifest // added SignupActivity to this file
    login.xml //changed the text below the EditText
    login // added a new method that is meant to be used to go to the signup view but it is broken

--- app/src/main/java/com/example/spotify_encore/login.java
+++ app/src/main/java/com/example/spotify_encore/login.java
@@ -1,7 +1,31 @@
 package com.example.spotify_encore;
 
-public class login {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 
 
 
+
+//      This method is meant to change the view when pressed
+    public void onClicksignup(View view) {
+//        Intent intent = new Intent(this, SignupActivity.class);
+//        startActivity(intent);
+
+
+
+
+       // startActivity(new Intent(this, SignupActivity.class));
+    }
 }
