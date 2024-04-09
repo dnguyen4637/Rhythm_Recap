886667130c52b382ca435c5542eb59274ea36ee0
Moses Adewolu <MosesOluwatobiAdewolu@gmail.com>
2024-04-09 16:23:39 -0400

    Changes

--- app/src/main/java/com/example/spotify_encore/login.java
+++ app/src/main/java/com/example/spotify_encore/login.java
@@ -1,6 +1,8 @@
 package com.example.spotify_encore;
 
-public class login {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
 
 
 
---
+++ app/src/main/java/com/example/spotify_encore/signIn.java
@@ -0,0 +1,15 @@
+package com.example.spotify_encore;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+}
