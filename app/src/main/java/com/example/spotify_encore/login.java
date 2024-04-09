0ed2b8ea17350c514620ae79245b8e2f8f596c5a
Nguyen, Dennis Duy Quang <dennisnguyen1475@gmail.com>
2024-04-09 17:38:20 -0400

    Setting up the Classes Login and Signup
    added the fields and later hopefully methods

--- app/src/main/java/com/example/spotify_encore/login.java
+++ app/src/main/java/com/example/spotify_encore/login.java
@@ -1,8 +1,16 @@
 package com.example.spotify_encore;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
+    private TextView textEmailLogin;
 
+    private TextView textPasswordLogin;
+
+    private EditText editEmailLogin;
+    private EditText editPasswordLogin;
 
 }
--- app/src/main/java/com/example/spotify_encore/signUp.java
+++ app/src/main/java/com/example/spotify_encore/signUp.java
@@ -1,15 +1,26 @@
 package com.example.spotify_encore;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signUp extends AppCompatActivity {
-
+    private TextView textUsernameSignUp;
+    private TextView textEmailSignUp;
+    private TextView textPasswordSignUp;
+    private EditText editUsernameSignUp;
+    private EditText editTextEmailSignUp;
+    private EditText editTextPasswordSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+    }
+
+    private void creatAccount() {
+        textUsernameSignUp = findViewById(R.id.textUsernameSignUp);
     }
 
 }
